122ec2665f6edf9adcd4b10513c9160f4df5f818
SavinaySingh <savinaydtu@gmail.com>
2024-10-06 21:25:02 +1100

    Updated Structure

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import pickle
+from collections import Counter
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from rapidfuzz import fuzz
+from src.restaurant_product_matching import RestaurantProductMatcher
+
+app = Flask(__name__)
+
+matcher = RestaurantProductMatcher('./pickle_files/restaurant_ingredients.pkl', './pickle_files/product_catalogue.pkl')
+matcher.calculate_cosine_similarity()
+matcher.fuzzy_cosine_similarity()
+matcher.compute_combined_scores()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    recommendations = None
+    if request.method == 'POST':
+        restaurant_name = request.form['restaurant_name']
+        recommendations = matcher.get_top_recommendations(restaurant_name)
+    
+        # Display results
+        if isinstance(recommendations, str):
+            print(recommendations)
+        else:
+            for recommendation in recommendations:
+                print(f"Product: {recommendation['Product']}")
+                print(f"Score: {recommendation['Score']:.2f}")
+                print(f"Ingredient Connection: {', '.join(recommendation['Ingredient Connection'])}")
+                print("\n---\n")
+    return render_template('index.html', recommendations=recommendations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
